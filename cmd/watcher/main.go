fe97d698be718919b7dd8eb4a1141690fa2ab6c7
Petr Zenin <mr@adminoid.com>
2024-12-01 08:21:33 +0300

    subscribeToTransactions decodes address from [32]uint8

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -4,7 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/tonkeeper/tonapi-go"
 )
+
+var addresses = []string{
+	"0:06d811f426598591b32b2c49f29f66c821368e4acb1de16762b04e0174532465",
+	"0:80d78a35f955a14b679faa887ff4cd5bfc0f43b4a4eea2a7e6927f3701b273c2",
+	"0:0e41dc1dc3c9067ed24248580e12b3359818d83dee0304fabcf80845eafafdb2",
+}
 
 func intPointer(x int) *int {
 	return &x
@@ -17,7 +24,7 @@ func subscribeToMempool(token string) {
 			// this "accounts" parameter is optional,
 			// if not set, you will receive all mempool events.
 			// if defined, you will receive only mempool events that involve these accounts.
-			[]string{"-1:5555555555555555555555555555555555555555555555555555555555555555"},
+			addresses,
 			func(data tonapi.MempoolEventData) {
 				value, _ := json.Marshal(data)
 				fmt.Printf("mempool event: %#v\n", value)
@@ -32,13 +39,23 @@ func subscribeToTransactions(token string) {
 	streamingAPI := tonapi.NewStreamingAPI(tonapi.WithStreamingToken(token))
 	for {
 		err := streamingAPI.SubscribeToTransactions(context.Background(),
-			[]string{"-1:5555555555555555555555555555555555555555555555555555555555555555"},
+			addresses,
 			// this "operations" is optional,
 			// if not set, you will receive all transactions.
 			// if defined, you will receive only transactions with these operations.
 			nil,
 			func(data tonapi.TransactionEventData) {
 				fmt.Printf("New tx with hash: %v\n", data.TxHash)
+				//fmt.Printf("%+v\n", data)
+				fmt.Printf("%#v\n", data)
+				s1 := data.AccountID.Workchain
+				fmt.Println(s1)
+				//s2 := string(bytes(data.AccountID.Address))
+				//s2 := fmt.Sprintf("%s", data.AccountID.Address)
+				//fmt.Println(s2)
+				fmt.Printf("%x\n", data.AccountID.Address)
+				fmt.Println(data.TxHash)
+				fmt.Println("----------")
 			})
 		if err != nil {
 			fmt.Printf("tx error: %v, reconnecting...\n", err)
@@ -49,9 +66,11 @@ func subscribeToTransactions(token string) {
 func subscribeToTraces(token string) {
 	streamingAPI := tonapi.NewStreamingAPI(tonapi.WithStreamingToken(token))
 	for {
-		err := streamingAPI.SubscribeToTraces(context.Background(), []string{"-1:5555555555555555555555555555555555555555555555555555555555555555"},
+		err := streamingAPI.SubscribeToTraces(context.Background(), addresses,
 			func(data tonapi.TraceEventData) {
 				fmt.Printf("New trace with hash: %v\n", data.Hash)
+				//fmt.Printf("%+v\n", data)
+				fmt.Printf("%#v\n", data)
 			})
 		if err != nil {
 			fmt.Printf("trace error: %v, reconnecting...\n", err)
@@ -82,11 +101,11 @@ func main() {
 	// To work with a local version of tonapi.io (opentonapi) use:
 	//         streamingAPI = tonapi.NewStreamingAPI(tonapi.WithStreamingEndpoint("http://127.0.0.1:8081"))
 	//
-	token := ""
+	token := "AHQGYRXC2JAB2TQAAAAG37FZFQA37NPCMJ65A6UDDTLZHPHJGWO7BZZYOX7I4OXE5MPT3DA"
 
-	go subscribeToTraces(token)
-	go subscribeToMempool(token)
+	//go subscribeToTraces(token)
+	//go subscribeToMempool(token)
 	go subscribeToTransactions(token)
-	go subscribeToBlocks(token)
+	//go subscribeToBlocks(token)
 	select {}
 }
